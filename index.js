c10f3b68792a0d42a26f4b8c78f30c57d8ce66b7
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-01-27 10:09:30 +0530

    updating

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ var Vehicle = require('vehicles-service');
 var list = require('vehicles-find');
 
 module.exports = function (ctx, container, options, done) {
-    var sandbox = container.sandbox;
     Vehicle.find({
-        query: options,
+        query: options.query,
         images: '288x162'
     }, function (err, vehicles) {
         if (err) {
             return done(err);
         }
-        list(ctx, sandbox, {
+        list(ctx, container, {
             vehicles: vehicles,
             title: 'Featured',
+            deck: options.deck,
             size: 4
         }, done);
     });
